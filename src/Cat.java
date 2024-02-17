ea8e146f7eeba326fb6e90d02432f23e0c3e36b5
Jackson <Jackson@dbacksys.com>
2024-02-16 20:29:48 -0700

    Changed the string to 'help àgit add srcgit add src'

--- src/Cat.java
+++ src/Cat.java
@@ -44,6 +44,7 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("help ඞඞඞඞඞඞඞඞඞඞඞඞඞ!!!!", catX, catY+HEAD_DIMENSION+30);   
+		//changed from Meow to that	
 	}
 }
