803448f0bf11bfa70e4dd371ff1cdbb46a5b695e
cdiamond1 <cdiamond1@mines.edu>
2024-02-16 20:19:12 -0700

    Changed body color from black to grey

--- src/Cat.java
+++ src/Cat.java
@@ -27,7 +27,7 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.black);
+		g2.setColor(Color.gray);	//Changed from black to grey
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.red);  //changed color to red from green
