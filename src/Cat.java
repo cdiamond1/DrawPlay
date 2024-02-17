5fa6152835789cec7cb12dbe50c993bac69cd60c
Jackson <Jackson@dbacksys.com>
2024-02-16 20:15:21 -0700

    Doubled 'MOUTH_HEIGHT

--- src/Cat.java
+++ src/Cat.java
@@ -14,7 +14,7 @@ public class Cat {
 	private static final int EYE_HEIGHT = 20;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
+	private static final int MOUTH_HEIGHT = 20; // changed from 10 to 20
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
